1a876c88104eaa5eb3c90bcc5578da7c59e61440
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-15 17:04:42 +0200

    added right class to the list item

--- js/script.js
+++ js/script.js
@@ -33,20 +33,89 @@ const root = new Vue ({
             {
               name: 'Stefano',
               avatar: '_2',
+              visible: true,
+              messages: [{
+                date: '10/01/2020 15:30:55',
+                text: 'Hola! come va? Birretta stasera?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                text: '18.30 al pub?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 16:15:22',
+                text: 'Stasera devo finire il progetto! Sarà per la prossima volta..',
+                status: 'received'
+              }
+              ],
             },
             {
               name: 'Giovanni',
               avatar: '_3',
+              visible: true,
+              messages: [{
+                date: '10/01/2020 15:30:55',
+                text: 'We zio! sabato prossimo serata?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                text: 'Ho visto che fanno un evento pazzesco a Milano',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 16:15:22',
+                text: 'Ci può stare! Perchè no!',
+                status: 'received'
+              }
+              ],
             },
             {
               name: 'Flavio',
               avatar: '_4',
+              visible: true,
+              messages: [{
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+              }
+              ],
             },
             {
                 name: 'Cristiano',
                 avatar: '_5',
+                visible: true,
+              messages: [{
+                date: '10/01/2020 15:30:55',
+                text: 'Hai portato a spasso il cane?',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 15:50:00',
+                text: 'Ricordati di dargli da mangiare',
+                status: 'sent'
+              },
+              {
+                date: '10/01/2020 16:15:22',
+                text: 'Tutto fatto!',
+                status: 'received'
+              }
+              ],
             }
           ]
-    }
+    },
+
 });
 
