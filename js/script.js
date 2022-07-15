49677fd9a8f854337afd3733bedffaaedf537482
Gabri1391 <chiarellogabriele@gmail.com>
2022-07-15 09:12:09 +0200

    MILESTONE

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+const root = new Vue ({
+    el:'#root',
+    data:{
+        user:{
+            name: 'Gabriele Chiarello',
+            avatar: '_io'
+        },
+        contacts: [
+            {
+              name: 'Flavio',
+              avatar: '_1',
+            },
+            {
+              name: 'Stefano',
+              avatar: '_2',
+            },
+            {
+              name: 'Giovanni',
+              avatar: '_3',
+            },
+            {
+              name: 'Alessia',
+              avatar: '_4',
+            },
+            {
+                name: 'Cristiano',
+                avatar: '_5',
+            }
+          ]
+    }
+});
+
